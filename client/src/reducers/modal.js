de2fcb5911ecdf575ddfd7d346d2b8184cf532e5
Tim Hitchins <tim.x.hitchins@gmail.com>
2020-10-02 17:05:16 -0700

    last commit before branching to test map

--- client/src/reducers/modal.js
+++ client/src/reducers/modal.js
@@ -2,7 +2,7 @@ import { TOGGLE_MODAL, TOGGLE_MODAL_OPTIONS } from "../actions/modal";
 
 const initialModalState = {
   selection: "Search",
-  isOpen: true,
+  isOpen: false,
 };
 
 export default function modal(state = initialModalState, action) {
