06cb8489bfb835d6dc1d00ddd567cdb42e33157c
pjsier <pjsier@gmail.com>
2024-06-30 07:36:00 -0400

    build: update sentry env

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express()
 
 Sentry.init({
   dsn: "https://66094b96912bcb73c3fcfa41b394e9bb@o4506586694025216.ingest.sentry.io/4506565004754944",
-  environment: process.env.ENVIRONMENT,
+  environment: process.env.NODE_ENV,
 })
 
 app.use(Sentry.Handlers.requestHandler())
